7224b0d4ea8cf4f1569be6a13a5b2b1892586f01
CrazyMachine2 <andres03@abv.bg>
2021-03-11 21:36:44 +0100

    init js + small refactoring

---
+++ solution/js/index.js
@@ -0,0 +1,33 @@
+
+main();
+
+function main() {
+    bookmark();
+
+};
+
+
+function bookmark() {
+    const button = document.querySelector(".bookmark");
+    const bookmarkImg = button.querySelector("img");
+    const btnText = button.querySelector(".btn-text");
+    
+    button.addEventListener("click", function(ev) {
+        const isBookmarked = button.classList.contains("is-bookmarked");
+        button.removeChild(bookmarkImg);
+
+        if(isBookmarked) {
+            button.classList.remove("is-bookmarked");
+            bookmarkImg.src = "../../images/icon-bookmark.svg";
+            btnText.textContent = "Bookmark";
+        } else {
+            button.classList.add("is-bookmarked");
+            bookmarkImg.src = "../../images/icon-bookmark-checked.svg"
+            btnText.textContent = "Bookmarked";
+        }
+
+        button.prepend(bookmarkImg);
+    })
+}
+
+
